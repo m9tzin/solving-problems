d9f699cd5b3f0c32672041a17c7bbfdf6feb6057
m9tzin <matheusmrno@gmail.com>
2025-03-28 21:08:03 -0300

    palindrome-number

--- leetcode/09-palindrome-number.py
+++ leetcode/09-palindrome-number.py
@@ -1,5 +1,15 @@
+# 1221 is a palindrome
+# 1212 isnt a palindrome
+
 def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
+        if x < 0:
+                return False
+        num = x
+        reversed_num = 0       
+        while x > 0:
+                last_digit = x % 10
+                reversed_num = reversed_num * 10 + last_digit
+                x = x // 10
+        return num == reversed_num
+
+        
