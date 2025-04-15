3c8c12af52c738e8efeb0b3d28bbd75bdf2e1775
Matheus Marinho <107821264+m9tzin@users.noreply.github.com>
2025-04-15 17:36:25 +0000

    two-sum

--- leetcode/01-two-sum.py
+++ leetcode/01-two-sum.py
@@ -1,9 +1,14 @@
-# two-sum problem
-
-def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
+# 01. Two Sum
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+# You can return the answer in any order.
+# You can follow up with other solutions, such as using a hash map or two pointers.
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
-                    return [i, j]
-    return None  
+                    return [i,j]
+        return None
+        
