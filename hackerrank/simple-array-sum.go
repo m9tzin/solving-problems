553a2365db3d03a6173bf1de91cccbcab57d0973
m9tzin <matheusmrno@gmail.com>
2025-08-13 16:23:47 -0300

    o/: first hackerrank problem

---
+++ hackerrank/simple-array-sum.go
@@ -0,0 +1,22 @@
+/* Given an array of integers, find the sum of its elements. */
+
+package main
+
+import (
+	"fmt"
+)
+
+func simpleArraySum(arr []int32) int32 {
+	var sum int32 = 0
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	}
+	fmt.Println(sum) // Print the sum
+	return sum
+}
+
+// Main func!
+func main() {
+	arr := []int32{1, 2, 3, 4, 5}
+	simpleArraySum(arr)
+}
